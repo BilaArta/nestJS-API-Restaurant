fd5bf0bf68cb5a8d72cb13679881d6fc30ad3430
BilaArta <bilaarta@gmail.com>
2022-03-23 19:45:13 -0700

    create subschema User on Transaction document

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -6,7 +6,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from "mongoose";
 import {Transaction, TransactionDocument} from "./schemas/transaction.schemas"
 
-
 @Injectable()
 export class TransactionService {
   constructor(@InjectModel(Transaction.name) private transactionModel: Model<TransactionDocument>) {}
